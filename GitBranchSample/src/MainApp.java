3101b89393804ee91f1fb3716683f0e70a509577
DESKTOP-7T3DR0I\銘古 <kuea8876@gmail.com>
2021-08-13 10:32:36 +0800

    1st Version

---
+++ GitBranchSample/src/MainApp.java
@@ -0,0 +1,13 @@
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		System.out.println("1st Version");
+	}
+}
+
+class Math {
+	public int add(int a, int b) {
+		return a + b;
+	}
+}
